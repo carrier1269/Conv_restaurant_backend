ae4cb0022c0466abf5687dd307afd734a4ebdc49
carrier1269 <carrier1269@naver.com>
2023-12-18 11:58:39 +0900

    first commit

---
+++ conv_restaurant/src/main/java/com/chlrkdls1269/conv_restaurant/ConvRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.chlrkdls1269.conv_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvRestaurantApplication.class, args);
+	}
+
+}
---
+++ conv_restaurant/src/test/java/com/chlrkdls1269/conv_restaurant/ConvRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chlrkdls1269.conv_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
